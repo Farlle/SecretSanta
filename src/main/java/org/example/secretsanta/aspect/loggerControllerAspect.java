fff75a77d880923fb6ca49e6c309cf8f018cde9b
Sashok <zh.alexander.qq@gmail.com>
2024-03-11 03:15:58 +0300

    добавил логгер для контроллеров

---
+++ src/main/java/org/example/secretsanta/aspect/loggerControllerAspect.java
@@ -0,0 +1,23 @@
+package org.example.secretsanta.aspect;
+
+
+import java.util.logging.Logger;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.springframework.stereotype.Component;
+
+
+@Aspect
+@Component
+public class loggerControllerAspect {
+
+        private static final Logger logger = Logger.getLogger(loggerControllerAspect.class.getName());
+
+        @After("execution(* org.example.secretsanta.controller.*.*(..))")
+        public void logAfter(JoinPoint joinPoint) {
+            logger.info("Method executed  " + joinPoint.getSignature().getName());
+        }
+
+}
