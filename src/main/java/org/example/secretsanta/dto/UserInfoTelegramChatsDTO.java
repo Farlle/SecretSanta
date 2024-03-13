a683f12c39ff9197c74bf1facdc6b02fc80af522
Sashok <zh.alexander.qq@gmail.com>
2024-03-13 03:14:47 +0300

    добавил классы с чатом телеграма

--- src/main/java/org/example/secretsanta/dto/UserInfoTelegramChatsDTO.java
+++ src/main/java/org/example/secretsanta/dto/UserInfoTelegramChatsDTO.java
@@ -1,2 +1,43 @@
-package org.example.secretsanta.dto;public class UserInfoTelegramChatsDTO {
+package org.example.secretsanta.dto;
+
+import org.example.secretsanta.model.entity.UserInfoEntity;
+
+public class UserInfoTelegramChatsDTO {
+
+    private int idUserInfoTelegramChat;
+    private int idUserInfo;
+    private int idChat;
+    private UserInfoEntity userInfoEntity;
+
+    public int getIdUserInfoTelegramChat() {
+        return idUserInfoTelegramChat;
+    }
+
+    public void setIdUserInfoTelegramChat(int idUserInfoTelegramChat) {
+        this.idUserInfoTelegramChat = idUserInfoTelegramChat;
+    }
+
+    public int getIdUserInfo() {
+        return idUserInfo;
+    }
+
+    public void setIdUserInfo(int idUserInfo) {
+        this.idUserInfo = idUserInfo;
+    }
+
+    public int getIdChat() {
+        return idChat;
+    }
+
+    public void setIdChat(int idChat) {
+        this.idChat = idChat;
+    }
+
+    public UserInfoEntity getUserInfoEntity() {
+        return userInfoEntity;
+    }
+
+    public void setUserInfoEntity(UserInfoEntity userInfoEntity) {
+        this.userInfoEntity = userInfoEntity;
+    }
 }
--- src/main/java/org/example/secretsanta/model/entity/UserInfoTelegramChats.java
+++ src/main/java/org/example/secretsanta/model/entity/UserInfoTelegramChats.java
@@ -1,2 +1,67 @@
-package org.example.secretsanta.model.entity;public class UserInfoTelegramChats {
+package org.example.secretsanta.model.entity;
+
+import javax.naming.Name;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user_info_telegram_chats")
+public class UserInfoTelegramChats {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_user_info_telegram_chat")
+    private int idUserInfoTelegramChat;
+
+    @Column(name = "id_user_info")
+    private int idUserInfo;
+
+    @Column(name = "id_chat")
+    private int idChat;
+
+    @OneToOne
+    @JoinColumn(name = "id_user_info", referencedColumnName = "id_user_info")
+    private UserInfoEntity userInfo;
+
+    public UserInfoTelegramChats(int idUserInfoTelegramChat, int idUserInfo, int idChat, UserInfoEntity userInfo) {
+        this.idUserInfoTelegramChat = idUserInfoTelegramChat;
+        this.idUserInfo = idUserInfo;
+        this.idChat = idChat;
+        this.userInfo = userInfo;
+    }
+
+    public UserInfoTelegramChats() {
+
+    }
+
+    public int getIdUserInfoTelegramChat() {
+        return idUserInfoTelegramChat;
+    }
+
+    public void setIdUserInfoTelegramChat(int idUserInfoTelegramChat) {
+        this.idUserInfoTelegramChat = idUserInfoTelegramChat;
+    }
+
+    public int getIdUserInfo() {
+        return idUserInfo;
+    }
+
+    public void setIdUserInfo(int idUserInfo) {
+        this.idUserInfo = idUserInfo;
+    }
+
+    public int getIdChat() {
+        return idChat;
+    }
+
+    public void setIdChat(int idChat) {
+        this.idChat = idChat;
+    }
+
+    public UserInfoEntity getUserInfo() {
+        return userInfo;
+    }
+
+    public void setUserInfo(UserInfoEntity userInfo) {
+        this.userInfo = userInfo;
+    }
 }
